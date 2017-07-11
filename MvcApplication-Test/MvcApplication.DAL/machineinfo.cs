54a6015dc55b001d7b2f5b9006075a5e448ba67a
liangwanzhuang <184533033@qq.com>
2017-07-11 21:01:30 +0800

    fix
    1.fix UI for procreate view（datepicker 、swalert、lodaling）
    2.fix inert  method for peoCreate Controller
    3.create  read code for pro-Index

--- MvcApplication-Test/MvcApplication.DAL/machineinfo.cs
+++ MvcApplication-Test/MvcApplication.DAL/machineinfo.cs
@@ -21,5 +21,6 @@ namespace MvcApplication.DAL
         public string other { get; set; }
         public Nullable<System.DateTime> createtime { get; set; }
         public Nullable<System.DateTime> fixtime { get; set; }
+        public Nullable<int> proID { get; set; }
     }
 }
